3323fc9aeaf516864b8aae5deb862e617e350bb0
Rahul Shakya <rahul.shakya3@gmail.com>
2018-08-20 17:15:23 +0530

    Offline capability
    Added offline capabilities for Firebase and Picasso Library

---
+++ app/src/main/java/com/example/asus/ebill/EBill.java
@@ -0,0 +1,31 @@
+package com.example.asus.ebill;
+
+import android.app.Application;
+
+import com.google.firebase.database.FirebaseDatabase;
+import com.squareup.picasso.OkHttp3Downloader;
+import com.squareup.picasso.Picasso;
+
+public class EBill extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+
+        //for Firebase offline capabilities
+
+        FirebaseDatabase.getInstance().setPersistenceEnabled(true);
+
+        //For Picasso offline capability
+
+        Picasso.Builder builder = new Picasso.Builder(this);
+        builder.downloader(new OkHttp3Downloader(this, Integer.MAX_VALUE));
+        Picasso built = builder.build();
+        built.setIndicatorsEnabled(true);
+        built.setLoggingEnabled(true);
+        Picasso.setSingletonInstance(built);
+
+
+    }
+}
--- app/src/main/java/com/example/asus/ebill/OrderProfileActivity.java
+++ app/src/main/java/com/example/asus/ebill/OrderProfileActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.google.firebase.database.DataSnapshot;
@@ -11,6 +12,7 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
 
 public class OrderProfileActivity extends AppCompatActivity {
 
@@ -26,6 +28,8 @@ public class OrderProfileActivity extends AppCompatActivity {
     private TextView s_time;
     private TextView e_time;
     private TextView pay_status;
+    private TextView image_url;
+    private ImageView profile_image;
 
     private DatabaseReference mOrdersDatabase;
 
@@ -56,6 +60,7 @@ public class OrderProfileActivity extends AppCompatActivity {
         s_time=findViewById(R.id.tv10);
         e_time=findViewById(R.id.tv2);
         pay_status=findViewById(R.id.tv12);
+        profile_image=findViewById(R.id.pro_image);
 
         mOrdersDatabase.addValueEventListener(new ValueEventListener() {
             @Override
@@ -71,6 +76,7 @@ public class OrderProfileActivity extends AppCompatActivity {
                 String ms_time=dataSnapshot.child("s_time").getValue().toString();
                 String me_time=dataSnapshot.child("e_time").getValue().toString();
                 String mpay_status=dataSnapshot.child("pay_status").getValue().toString();
+                String dp=dataSnapshot.child("image").getValue().toString();
 
                 curr_time.setText(time);
                 addr.setText(add);
@@ -81,6 +87,7 @@ public class OrderProfileActivity extends AppCompatActivity {
                 s_time.setText(ms_time);
                 e_time.setText(me_time);
                 pay_status.setText(mpay_status);
+                Picasso.get().load(dp).into(profile_image);
 
             }
 
